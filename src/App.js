edcd3a63ad5045068bf929c54f299a481067ed70
Kanykei <kanisatybaldyeva@gmail.com>
2023-08-13 21:58:47 +0600

    add useInput

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 
 import './App.css';
+import { useInput } from './hooks/useInput';
 
 
 function App() {
- 
+ const name = useInput('')
   return (
     <div className="App">
-     
+     <form>
+        <input onChange={e=>name.onChange(e)} onBlur={name.onBlur} value={name.value}/>
+     </form>
     </div>
   );
 }
